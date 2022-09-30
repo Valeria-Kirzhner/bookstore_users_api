b6a648d960c3de0fc1b9fd84aa2215081472ec95
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2022-09-30 23:49:51 +0300

    local memory replaced with use of mysql database for user saving proccess.

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -6,8 +6,12 @@ import (
 	"bookstore_users_api/utils/date_utils"
 	"bookstore_users_api/utils/errors"
 	"fmt"
+	"strings"
 )
-
+const (
+	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?,?,?,?);"
+	duplicatedEntry = "1062"
+)
 var ( usersDB = make(map[int64]*User))
 
 func (user *User) Get()  *errors.RestErr {
@@ -28,15 +32,28 @@ func (user *User) Get()  *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registred", user.Email))
 
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
-	user.DateCreated =  date_utils.GetNowString()
-	usersDB[user.Id] = user
+	defer stmt.Close()
+
+	user.DateCreated = date_utils.GetNowString()
+
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), duplicatedEntry) {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email ))
+			
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when truing to save user: %s", err.Error()))
+	}
+
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("error when truing to save user: %s", err.Error()))
+	}
+	user.Id = userId
 	return nil
 }
